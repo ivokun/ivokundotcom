83dfa47a76f0d846cb42ceaa387bdfcc6c9c6817
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2022-11-18 17:55:05 +0700

    feat: initial commit

---
+++ api/config/middlewares.js
@@ -0,0 +1,37 @@
+module.exports = ({ env }) => ([
+  "strapi::errors",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            env('R2_PUBLIC_URL').replace(/^https?:\/\//, ""), // removes http or https from url
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            env('R2_PUBLIC_URL').replace(/^https?:\/\//, ""),
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+]);
---
+++ api/config/plugins.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-cloudflare-r2",
+      providerOptions: {
+        accessKeyId: env('R2_ACCESS_KEY_ID'),
+        secretAccessKey: env('R2_ACCESS_SECRET'),
+        region: "us-east-1",
+        params: {
+          Bucket: "ivokun-prod",
+          accountId: env('R2_ACCOUNT_ID'),
+          publicUrl: env('R2_PUBLIC_URL'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
