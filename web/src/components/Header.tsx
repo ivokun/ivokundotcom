904cecd058c560999944a9dca1f211a10ffeb01f
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2023-03-23 10:40:50 +0700

    chore: remove Me

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,18 +1,33 @@
 export function Header() {
   return (
-    <header className="mt-12">
-      <nav className="grid grid-cols-10 justify-items-center">
-        <div className="col-span-7 justify-self-start text-lg">IVOKUN</div>
-
-        <a href="/" className="text-lg text-blue-600 hover:text-blue-800">
-          Home
-        </a>
-        <a href="/me" className="text-lg text-blue-600 hover:text-blue-800">
-          Me
-        </a>
-        <a href="/posts" className="text-lg text-blue-600 hover:text-blue-800">
-          Posts
-        </a>
+    <header className="py-12">
+      <nav className="grid grid-cols-2">
+        <div className="justify-self-start text-lg">IVOKUN</div>
+        <div className="justify-self-end">
+          <ul className="flex space-x-4">
+            <li>
+              <a href="/" className="text-lg text-blue-600 hover:text-blue-800">
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="/posts"
+                className="text-lg text-blue-600 hover:text-blue-800"
+              >
+                Posts
+              </a>
+            </li>
+            <li>
+              <a
+                href="/galleries"
+                className="text-lg text-blue-600 hover:text-blue-800"
+              >
+                Galleries
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
