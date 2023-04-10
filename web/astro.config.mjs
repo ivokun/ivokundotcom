23ffe0741d5ffe641c7a74d2e56bd3d1fd042e8b
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2023-04-11 05:32:31 +0700

    feat: add prettier and eslint

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: "https://ivokun.com"
+  site: 'https://ivokun.com',
 });
