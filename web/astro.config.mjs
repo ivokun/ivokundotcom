69e42b7d736e48df4309dd58f8154faeb2ceabb4
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2024-01-03 23:36:45 +0000

    Feat/update ui

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+const { CMS_API_URL } = loadEnv(process.env.CMS_API_URL, process.cwd(), '');
+const { CMS_API_TOKEN } = loadEnv(process.env.CMS_API_TOKEN, process.cwd(), '');
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  env: {
+    CMS_API_URL,
+    CMS_API_TOKEN,
+  },
   site: 'https://ivokun.com',
 });
--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,28 +1,9 @@
 export function Header() {
   return (
-    <header className="py-12">
-      <nav className="grid grid-cols-2">
-        <div className="justify-self-start text-lg">IVOKUN</div>
-        <div className="justify-self-end">
-          <ul className="flex space-x-4">
-            <li>
-              <a href="/" className="text-lg">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="/posts" className="text-lg">
-                Posts
-              </a>
-            </li>
-            <li>
-              <a href="/galleries" className="text-lg">
-                Galleries
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <header className="fixed top-4 left-4 lg:left-auto">
+      <a href="/">
+        <img src="/ivokun_logo.png" width={50} />
+      </a>
     </header>
   );
 }
